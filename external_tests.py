23b83d662876c7b87a1a86fc5c3ec6ec6ced2c24
Jenni Schofield <jennischofield02@icloud.com>
2020-12-19 18:09:40 +0000

    Update external_tests.py

--- external_tests.py
+++ external_tests.py
@@ -1,6 +1,7 @@
 from alarm_clock import get_covid, get_weather, get_headlines, news_articles
 
 def test_external():
+    """this tests whether or not the api functions correctly gather the data"""
     get_headlines()
     assert isinstance(get_covid(),dict)
     assert isinstance(get_weather(), dict)
